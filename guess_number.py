d02b5eb778b0b522b22b5d5bc2ba22f4a059d0db
Elio <etigersoft@icloud.com>
2022-03-03 08:10:15 -0500

    Crafted a guess number video game

---
+++ guess_number.py
@@ -0,0 +1,33 @@
+# Description: 
+#   Game: Guess the number
+# Author: 
+#   Elio Garcia
+# Author GitHub profile: 
+#   https://github.com/etigersoft/
+
+from random import randint
+
+def run():
+    seed_number = randint(1,50)
+    guess_number = int(input("Guess the number from 1 to 50 in 5 tries max: "))
+    tries = 1
+
+    while seed_number != guess_number and tries < 5:
+        if guess_number < seed_number:
+            print("Hint: Try a bigger number")
+        else:
+            print("Hint: Try a smaller number")
+        
+        if tries == 4: 
+            print("(Your las try)")
+
+        guess_number = int(input("Guess a diferent number: "))
+        tries += 1
+
+    if seed_number == guess_number:
+        print("Congratulations, you got the number in " + str(tries) + " tries")
+    else:
+        print("You have reached the max number of tries!")
+
+if __name__ == "__main__":
+    run()
